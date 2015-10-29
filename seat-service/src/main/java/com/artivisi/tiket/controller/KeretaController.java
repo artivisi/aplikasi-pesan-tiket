08ee03a4e1a8da17c04d847254eb80c622bad639
Endy Muhardin <endy.muhardin@gmail.com>
2015-10-29 09:55:23 +0700

    implement save dan update

--- seat-service/src/main/java/com/artivisi/tiket/controller/KeretaController.java
+++ seat-service/src/main/java/com/artivisi/tiket/controller/KeretaController.java
@@ -9,9 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.http.HttpStatus;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
+
+import javax.validation.Valid;
 
 @RestController
 public class KeretaController {
@@ -21,5 +28,19 @@ public class KeretaController {
     @RequestMapping(value="/kereta", method=RequestMethod.GET)
     public Page<Kereta> daftarKereta(Pageable pr){
         return dao.findAll(pr);
+    }
+
+    
+    @RequestMapping(value="/kereta", method=RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createKereta(@RequestBody @Valid Kereta k){
+        dao.save(k);
+    }
+    
+    @RequestMapping(value="/kereta/{id}", method=RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.OK)
+    public void updateKereta(@PathVariable String id, @RequestBody @Valid Kereta k){
+        k.setId(id);
+        dao.save(k);
     }
 }
--- seat-service/src/main/java/com/artivisi/tiket/entity/Kereta.java
+++ seat-service/src/main/java/com/artivisi/tiket/entity/Kereta.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Id;
 import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import org.hibernate.annotations.GenericGenerator;
+
 
 import javax.validation.constraints.Size;
 
@@ -12,12 +15,22 @@ import javax.validation.constraints.Size;
 public class Kereta {
 
     @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name="uuid",strategy="uuid2")
     private String id;
 
     @Column(unique=true, nullable=false, name="kode")
     @Size(min=3, max=10)
     private String kode;
     private String nama;
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
 
     public String getKode(){
         return kode;
